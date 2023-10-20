32d28dff2fc3c68d1d62305501a2d610a8530dab
DannyHillCurated <112188723+dannyhill98@users.noreply.github.com>
2023-10-20 17:01:28 -0400

    init

--- api/index.js
+++ api/index.js
@@ -1,20 +1,9 @@
+import http from 'http';
+import app from './app.js';
 
-import express from "express";
-import dotenv from 'dotenv';
-import cors from 'cors';
-import authRoutes from "./routes/auth.route.js";
-import trackingRoutes from "./routes/tracking.route.js";
+const server = http.createServer(app);
+const port = process.env.PORT ?? 3001;
 
-dotenv.config();
-
-const app = express();
-
-app.use(cors());
-
-app.use(express.json());
-
-app.get('/', (req, res) => {res.send('Hello World!')})
-
-app.use(authRoutes,trackingRoutes);
-
-export default app;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
