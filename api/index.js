5d92a66bfb6e826385a4fb5329a1dca099bb5fc5
DannyHillCurated <112188723+dannyhill98@users.noreply.github.com>
2023-10-21 12:37:59 -0400

    init

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (req, res) => {res.send('Hello World!')})
+app.get('/', (req, res) => {res.send('bac_test api')})
 
 app.use(authRoutes,trackingRoutes);
 
